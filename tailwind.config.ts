cea7ab55d20739e865d19094af16ef1a03ea9f6a
Collection50 <fkaus65@naver.com>
2024-07-08 15:21:36 +0900

    chore: eslint, package 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import type {Config} from "tailwindcss";
 import {nextui} from "@nextui-org/react";
 
@@ -7,9 +8,38 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+=======
+import type { Config } from 'tailwindcss'
+
+const px0To10 = {
+  ...Array.from(Array(11)).reduce(
+    (acc, _, i) => ({ ...acc, [i]: `${i}px` }),
+    {},
+  ),
+}
+const px0To100 = {
+  ...Array.from(Array(101)).reduce(
+    (acc, _, i) => ({ ...acc, [i]: `${i}px` }),
+    {},
+  ),
+}
+const px0To500 = {
+  ...Array.from(Array(501)).reduce(
+    (acc, _, i) => ({ ...acc, [i]: `${i}px` }),
+    {},
+  ),
+}
+
+const config: Config = {
+  content: [
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+>>>>>>> 22df70a (chore: eslint, package 설정)
   ],
   theme: {
+    colors: { transparent: 'transparent' },
     extend: {
+<<<<<<< HEAD
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
@@ -20,3 +50,20 @@ const config: Config = {
   plugins: [nextui()]
 };
 export default config;
+=======
+      width: px0To500,
+      height: px0To500,
+      borderWidth: px0To10,
+      fontSize: px0To100,
+      lineHeight: px0To100,
+      minWidth: px0To500,
+      minHeight: px0To500,
+      spacing: px0To500,
+      borderRadius: { ...px0To100, button: 6 },
+    },
+  },
+  plugins: [],
+}
+
+export default config
+>>>>>>> 22df70a (chore: eslint, package 설정)
