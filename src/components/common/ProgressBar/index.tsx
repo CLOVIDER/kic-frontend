b9659ff20785a2b19359b5cb0ae50395faee6ab6
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-07-30 02:29:04 +0900

    feat: 관리자 랜딩페이지 구현

--- src/components/common/ProgressBar/index.tsx
+++ src/components/common/ProgressBar/index.tsx
@@ -15,7 +15,7 @@ export default function ProgressBar({ label, value, bar }: ProgressBarProps) {
       </div>
       <div className="flex items-center w-full mt-2">
         <div
-          className={cn(`h-15 rounded-r-full bg-[#000000]`, bar)}
+          className={cn('h-15 rounded-r-full bg-[#000000]', bar)}
           style={{ width: `${(value / 10) * 100}%` }}
         />
         <span className="ml-4 font-['Cairo'] font-bold text-[#6d6c6c] text-20">
--- src/components/common/StatusBox/index.tsx
+++ src/components/common/StatusBox/index.tsx
@@ -1,14 +1,26 @@
 'use client'
 
 import { ReactNode } from 'react'
+import { cn } from '@/util'
 import { useStatusBox } from './useStatusBox'
 
-export default function StatusBox({ children }: { children: ReactNode }) {
+export default function StatusBox({
+  children,
+  className,
+}: {
+  children?: ReactNode
+  className?: string
+}) {
   const { currentStatus, currentPeriod, dDay } = useStatusBox()
   return (
-    <div className="w-[664px] h-295 rounded-32 bg-gradient-01 px-50 flex flex-col">
-      <div className="flex flex-row items-center gap-20">
-        <p className="font-sans font-bold text-90">D-{dDay}</p>
+    <div
+      className={cn(
+        'w-[664px] h-295 rounded-32 bg-gradient-01 px-50 flex flex-col justify-center gap-20',
+        className,
+      )}
+    >
+      <div className="flex flex-row items-center gap-20 h-80">
+        <span className="font-sans font-semibold text-80">D-{dDay}</span>
         <div className="flex flex-col leading-35 pt-10">
           <div className="text-35 font-medium">{currentStatus}</div>
           <div className="text-20">
