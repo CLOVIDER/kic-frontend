8ea7bc91e4810ff5dc015c3705646a353c5e04e3
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-07-30 15:48:51 +0900

    chore

--- src/app/(auth)/auth/api/queries.ts
+++ src/app/(auth)/auth/api/queries.ts
@@ -1,10 +1,13 @@
 import { useMutation } from '@tanstack/react-query'
 import { ACCESS_TOKEN, ROLE } from '@/constants'
 import Cookies from 'js-cookie'
+import { useRouter } from 'next/navigation'
 import { postLogin } from '.'
 import { LoginRequest } from './type'
 
 export const usePostLogin = (data: LoginRequest) => {
+  const { push } = useRouter()
+
   return useMutation({
     mutationKey: ['login'],
     mutationFn: () => postLogin(data),
@@ -12,6 +15,7 @@ export const usePostLogin = (data: LoginRequest) => {
     onSuccess: ({ result: { accessToken, role } }) => {
       Cookies.set(ACCESS_TOKEN, accessToken)
       Cookies.set(ROLE, role)
+      push('/')
     },
   })
 }
