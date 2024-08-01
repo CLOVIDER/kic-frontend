54d5ce831c468952180b088776464fe9e57cdc63
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-08-01 11:41:20 +0900

    chore

--- src/components/common/Header/Logo/index.tsx
+++ src/components/common/Header/Logo/index.tsx
@@ -7,10 +7,15 @@ export default function Logo() {
 
   return (
     <div className="text-12 flex flex-row gap-10">
-      <div className="flex flex-row items-center gap-5">
-        <Image src="/images/logo.png" alt="logo" width={18} height={13} />
-        <p>kids in company</p>
-      </div>
+      <Button
+        className="w-15 h-15 text-[12px] bg-transparent text-[#333333]"
+        onClick={() => push('/')}
+      >
+        <div className="flex flex-row items-center gap-5 whitespace-nowrap">
+          <Image src="/images/logo.png" alt="logo" width={18} height={13} />
+          <p>kids in company</p>
+        </div>
+      </Button>
       <Button
         className="w-15 h-15 text-[12px] bg-transparent text-[#333333]"
         onClick={() => push('/login')}
