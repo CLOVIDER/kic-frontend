51b90f100a60a2b081d551e874c599d400a07234
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-08-01 09:09:09 +0900

    chore

--- src/app/lib/QueryProvider.tsx
+++ src/app/lib/QueryProvider.tsx
@@ -1,15 +1,17 @@
 'use client'
 
-import { ReactNode, useState } from 'react'
+import { useState } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { StrictPropsWithChildren } from '@/type'
 
-export default function QueryProvider({ children }: { children: ReactNode }) {
+export default function QueryProvider({ children }: StrictPropsWithChildren) {
   const [queryClient] = useState(
     () =>
       new QueryClient({
         defaultOptions: {
           queries: {
             staleTime: 60 * 1000,
+            retry: false,
           },
         },
       }),
