a6b3dcd8f01859fb6109699fb9233aee1b221917
mango0422_mac <syj0422@gachon.ac.kr>
2024-08-01 13:16:10 +0900

    🐛 CLO-111 fix layout

--- src/app/(admin)/admin/notice/layout.tsx
+++ src/app/(admin)/admin/notice/layout.tsx
@@ -1,22 +1,15 @@
-import { Metadata } from 'next'
+import { PropsWithChildren } from 'react'
+import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: '공지사항',
-  description: '공지사항',
+  title: 'notice (admin)',
+  description: 'notice list page for administrator',
 }
 
-export default function NoticeLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+type LayoutProps = PropsWithChildren<unknown>
+
+export default function Layout({ children }: LayoutProps) {
   return (
-    <div className="flex justify-center w-full h-full">
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, maximum-scale=5, minimum-scale=1, user-scalable=yes"
-      />
-      {children}
-    </div>
+    <main className="h-full flex justify-center items-center">{children}</main>
   )
 }
