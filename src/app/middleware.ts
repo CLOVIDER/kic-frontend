1f1b2888fdaba2bc7d271922b3c98215e6d73061
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-08-03 23:31:10 +0900

    feat: 페이지 접근 제한 구현

---
+++ src/app/middleware.ts
@@ -0,0 +1,34 @@
+import { ACCESS_TOKEN, ROLE } from '@/constants'
+import { NextRequest, NextResponse } from 'next/server'
+
+export const config = {
+  matcher: ['/((?!api|_next/static|lib|favicon.ico|fonts|images).*)'],
+}
+const protectedRoutes = ['/apply', '/apply/application']
+const publicRoutes = ['/login']
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get(ACCESS_TOKEN)
+  const role = request.cookies.get(ROLE)?.value
+  const currentPath = request.nextUrl.pathname
+
+  if (!token && protectedRoutes.includes(currentPath)) {
+    const url = request.nextUrl.clone()
+    url.pathname = '/'
+    return NextResponse.redirect(url)
+  }
+
+  if (token && publicRoutes.includes(currentPath)) {
+    const url = request.nextUrl.clone()
+    url.pathname = '/'
+    return NextResponse.redirect(url)
+  }
+
+  if (role === 'user' && currentPath.startsWith('/admin')) {
+    const url = request.nextUrl.clone()
+    url.pathname = '/'
+    return NextResponse.redirect(url)
+  }
+
+  return NextResponse.next()
+}
