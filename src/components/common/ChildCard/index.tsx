6b84eb4026c7a300a23da0e892d222605edb327e
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-08-06 11:02:39 +0900

    style

--- src/components/common/ChildCard/index.tsx
+++ src/components/common/ChildCard/index.tsx
@@ -1,11 +1,23 @@
+import { cn } from '@/lib/utils'
+
 interface ChildCardProps {
   name: string
   kindergartenList: string[]
+  className?: string
 }
 
-export default function ChildCard({ name, kindergartenList }: ChildCardProps) {
+export default function ChildCard({
+  name,
+  kindergartenList,
+  className,
+}: ChildCardProps) {
   return (
-    <div className="flex flex-col items-center w-200 h-117 bg-[#FECDA9]/40 rounded-8 shadow-sm border border-[#cccccc]">
+    <div
+      className={cn(
+        'flex flex-col items-center w-200 h-117 bg-[#FECDA9]/40 rounded-8 shadow-sm border border-[#cccccc]',
+        className,
+      )}
+    >
       <div className="my-10 text-[#5a5650] font-semibold underline">{name}</div>
       <ul className="list-disc list-inside text-[#666666] text-14">
         {kindergartenList.map((kindergarten) => (
