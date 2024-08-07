9e7d1c5195a5002cc69d777973ab8cedc912d722
Collection50 <fkaus65@naver.com>
2024-08-07 09:45:57 +0900

    fix

--- src/app/(admin)/admin/applications/layout.tsx
+++ src/app/(admin)/admin/applications/layout.tsx
@@ -1,13 +1,9 @@
-import { AsyncBoundaryWithQuery } from '@/react-utils'
 import { StrictPropsWithChildren } from '@/type'
-import KindergartensFetcher from './fetcher/KindergartensFetcher'
 
 export default function Layout({ children }: StrictPropsWithChildren) {
   return (
     <section className="w-full h-full flex justify-center items-center">
-      <AsyncBoundaryWithQuery>
-        <KindergartensFetcher>{children}</KindergartensFetcher>
-      </AsyncBoundaryWithQuery>
+      {children}
     </section>
   )
 }
