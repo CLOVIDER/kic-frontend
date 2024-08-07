9c921e72d10cbf852d18ec9cbae173192d1e3e6c
Collection50 <fkaus65@naver.com>
2024-08-07 09:50:05 +0900

    fix

--- src/app/(admin)/admin/applications/queries/index.ts
+++ src/app/(admin)/admin/applications/queries/index.ts
@@ -2,15 +2,7 @@ import { useSuspenseQuery } from '@tanstack/react-query'
 import {
   GetApplicationsRequest,
   getApplications,
-  getKindergartens,
 } from '@/app/(admin)/admin/applications/api/index'
-
-export const useGetKindergartens = () =>
-  useSuspenseQuery({
-    queryKey: ['get-kindergartens'],
-    queryFn: getKindergartens,
-    select: ({ result }) => result,
-  })
 
 export const useGetApplications = (applicationQuery: GetApplicationsRequest) =>
   useSuspenseQuery({
