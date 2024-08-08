be3e6279118cf72246cfa40935563f02c06a3df2
Collection50 <fkaus65@naver.com>
2024-08-08 10:45:06 +0900

    File

---
+++ src/components/common/FileEdit/index.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import { useRef, ChangeEvent, useCallback } from 'react'
+import Input from '../Input'
+import Button from '../Button'
+
+export default function FileEdit({
+  onFileSelect,
+}: {
+  onFileSelect: (file: File) => void
+}) {
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const handleFileSelect = (e: ChangeEvent<HTMLInputElement>) => {
+    const inputEl = e.target as HTMLInputElement
+
+    if (inputEl.files?.[0]) {
+      onFileSelect(inputEl.files[0])
+    }
+  }
+
+  const handleButtonClick = useCallback(() => {
+    if (inputRef.current) {
+      inputRef.current.click()
+    }
+  }, [])
+
+  return (
+    <div>
+      <Input
+        type="file"
+        accept="image/*"
+        id="profile"
+        className="hidden"
+        ref={inputRef}
+        onChange={handleFileSelect}
+      />
+      <Button
+        onClick={handleButtonClick}
+        className="w-70 m-5 p-4 bg-primary/30 text-primary"
+      >
+        edit
+      </Button>
+    </div>
+  )
+}
