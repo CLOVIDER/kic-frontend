5fc7bc2f798bb3661b0d0958bff5960bb850afdd
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-08-12 13:51:22 +0900

    fix

--- src/app/(admin)/admin/applications/layout.tsx
+++ src/app/(admin)/admin/applications/layout.tsx
@@ -2,8 +2,6 @@ import { StrictPropsWithChildren } from '@/type'
 
 export default function Layout({ children }: StrictPropsWithChildren) {
   return (
-    <section className="flex justify-center items-center">
-      {children}
-    </section>
+    <section className="flex justify-center items-center">{children}</section>
   )
 }
