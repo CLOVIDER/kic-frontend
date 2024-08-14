0380035162b8b4b7d22b8db03d64d7df6796499f
Collection50 <fkaus65@naver.com>
2024-08-14 15:49:01 +0900

    fix

--- src/app/(user)/lottery/page.tsx
+++ src/app/(user)/lottery/page.tsx
@@ -3,15 +3,15 @@
 import Image from 'next/image'
 import { AsyncBoundaryWithQuery } from '@/react-utils'
 import { motion } from 'framer-motion'
+import { If } from '@/components'
 import LotteryProcessing from './components/LotteryEntry/LotteryProcessing'
 import LotteryResult from './components/LotteryEntry/LotteryResult'
 import { ChildrenFetcher } from './fetcher/ChildrenFetcher'
 import { useRecruitStatusContext } from './fetcher/RecruitStatusFetcher'
-// import LotteryPending from './components/LotteryEntry/LotteryPending'
+import LotteryEnd from './components/LotteryEntry/LotteryEnd'
 
 export default function LotteryEntry() {
   const { recruitStatus } = useRecruitStatusContext()
-  const abc = '모집기간'
 
   return (
     <section className="flex">
@@ -35,18 +35,26 @@ export default function LotteryEntry() {
         />
       </motion.div>
 
-      {/* {recruitStatus === '모집예정' && <LotteryEnd />}
-      {recruitStatus === '모집없음' && <LotteryEnd />}
-      <LotteryPending /> */}
+      <If
+        condition={recruitStatus === '모집예정' || recruitStatus === '모집없음'}
+      >
+        <LotteryEnd />
+      </If>
 
-      <AsyncBoundaryWithQuery>
-        <ChildrenFetcher>
-          {abc === '모집기간' && <LotteryProcessing />}
+      <If
+        condition={['모집기간', '1차등록기간', '2차등록기간'].includes(
+          recruitStatus,
+        )}
+      >
+        <AsyncBoundaryWithQuery>
+          <ChildrenFetcher>
+            {recruitStatus === '모집기간' && <LotteryProcessing />}
 
-          {(recruitStatus === '1차등록기간' ||
-            recruitStatus === '2차등록기간') && <LotteryResult />}
-        </ChildrenFetcher>
-      </AsyncBoundaryWithQuery>
+            {(recruitStatus === '1차등록기간' ||
+              recruitStatus === '2차등록기간') && <LotteryResult />}
+          </ChildrenFetcher>
+        </AsyncBoundaryWithQuery>
+      </If>
     </section>
   )
 }
