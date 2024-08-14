11d05181cb32a343abfb9fdfe7b2a12041a9d7dc
Collection50 <fkaus65@naver.com>
2024-08-14 13:58:39 +0900

    fix

--- src/app/(user)/lottery/components/LotteryEntry/LotteryProcessing/index.tsx
+++ src/app/(user)/lottery/components/LotteryEntry/LotteryProcessing/index.tsx
@@ -4,13 +4,13 @@ import {
   DropdownMenu,
   DropdownTrigger,
 } from '@nextui-org/react'
-import { useFunnelContext } from '@/components'
+import { useRouter } from 'next/navigation'
 import LotteryCard from '../LotteryCard'
 import { useChildrenContext } from '../../../fetcher/ChildrenFetcher'
 
 export default function LotteryProcessing() {
   const { childrenInfo } = useChildrenContext()
-  const { setStep } = useFunnelContext()
+  const { push } = useRouter()
 
   return (
     <LotteryCard className="gap-11 text-center z-100">
@@ -23,26 +23,18 @@ export default function LotteryProcessing() {
       </LotteryCard.Description>
 
       <LotteryCard.Content className="flex flex-col gap-20 mt-71">
-        {childrenInfo.map(({ childName, recruitInfos }) => (
+        {childrenInfo.map(({ childName, className, lotteryId }) => (
           <Dropdown key={childName}>
             <DropdownTrigger>{childName}</DropdownTrigger>
 
             <DropdownMenu>
-              <DropdownMenu>
-                {recruitInfos.map(({ kindergartenNm, className }) => (
-                  <DropdownItem
-                    key={kindergartenNm}
-                    className='className="border-1 p-16 border-[#F90] rounded-16 w-221 h-56 bg-white text-20 text-[#F90]'
-                    onClick={() =>
-                      setStep('result', {
-                        query: { kindergartenNm, className },
-                      })
-                    }
-                  >
-                    {kindergartenNm} {className}
-                  </DropdownItem>
-                ))}
-              </DropdownMenu>
+              <DropdownItem
+                key={lotteryId}
+                className='className="border-1 p-16 border-[#F90] rounded-16 w-221 h-56 bg-white text-20 text-[#F90]'
+                onClick={() => push(`/lottery/${lotteryId}`)}
+              >
+                {className}
+              </DropdownItem>
             </DropdownMenu>
           </Dropdown>
         ))}
