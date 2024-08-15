80bb69d1f8a1b7a41f8772e1ef8450c15efbc860
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-08-15 21:18:02 +0900

    Clo-사용자-신청내역-라우팅-로직-변경 (#41)
    * chore
    
    * feat: get 메써드 연결
    
    * feat: image 파일 추가
    
    * feat: 애니메이션 추가
    
    * chore
    
    * feat: 관리자 승인, 사용자 신청내역 조회를 위한 application 컴포넌트 api 연결
    
    * refactor: 헤더 로직 변경
    
    * fix: 중복 렌더링 제거
    
    * style: ui 변경
    
    * feat: 관리자 네비게이션 추가
    
    * chore
    
    * refactor
    
    * chore
    
    * fix
    
    * feat: patch, post api 연결
    
    * refactor: 헤더 로직 변경
    
    * fix: 중복 렌더링 제거
    
    * style: ui 변경
    
    * feat: 관리자 네비게이션 추가
    
    * chore
    
    * refactor
    
    * Revert "refactor"
    
    This reverts commit 1dc120f0fd82d158efb996f5c6d99140cb42f805.
    
    * feat
    
    * feat
    
    * chore
    
    * chore
    
    * chore
    
    * fix
    
    * fix
    
    * style
    
    * refactor: 신청내역 불러오기 로직 변경

--- src/app/(user)/apply/page.tsx
+++ src/app/(user)/apply/page.tsx
@@ -1,35 +1,9 @@
-'use client'
-
-import React, { useEffect, useState } from 'react'
 import { ToastContainer } from 'react-toastify'
-import { useRouter } from 'next/navigation'
 import LeftSection from './components/LeftSection'
 import ApplicationForm from './components/ApplicationForm'
 import 'react-toastify/dist/ReactToastify.css'
-import { getApplicationData } from './api'
 
 export default function Page() {
-  const [loading, setLoading] = useState(true)
-  const router = useRouter()
-
-  useEffect(() => {
-    async function checkApplicationStatus() {
-      const applicationStatus = await getApplicationData()
-
-      if (applicationStatus.id === null) {
-        setLoading(false)
-      } else {
-        router.push('/apply/application')
-      }
-    }
-
-    checkApplicationStatus()
-  }, [router])
-
-  if (loading) {
-    return <div>Loading...</div>
-  }
-
   return (
     <div className="flex flex-row justify-center gap-220 mt-150 ml-190 w-full">
       <LeftSection />
