507ffd9d73b1a04d786752fe4cc82827068f3d8d
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-08-15 22:44:31 +0900

    style

--- src/app/(user)/apply/application/page.tsx
+++ src/app/(user)/apply/application/page.tsx
@@ -48,7 +48,7 @@ export default function Page() {
 
   const { push } = useRouter()
   return (
-    <div className="w-full h-full flex flex-col gap-5 relative p-100">
+    <div className="w-full h-full flex flex-col items-center gap-5 relative p-100">
       <Image
         src="/images/application-bg.svg"
         alt="bg"
@@ -67,33 +67,44 @@ export default function Page() {
       >
         이미 신청하신 내역이 있어요 !
       </h1>
-      <If condition={showComponents}>
-        <div className="ml-270 fade-in ease-in-out delay-300">
-          <Application type="user" />
-        </div>
-      </If>
 
-      <If condition={showComponents}>
-        <div className="flex flex-row items-center gap-10 ml-800 fade-in delay-600">
-          <CancelModal id={applicationId}>
-            {(onOpen) => (
+      <div className="w-800 flex flex-col items-center ml-50">
+        <If condition={showComponents}>
+          <div className="fade-in ease-in-out delay-300">
+            <Application type="user" />
+          </div>
+        </If>
+
+        <If condition={showComponents}>
+          <div className="flex w-full flex-row justify-between fade-in delay-600">
+            <Button
+              onClick={() => push('/')}
+              className="border-1 p-16 rounded-[20px] w-120 h-40 text-[18px] text-white bg-gradient-to-r from-[#ffbb37] to-[#ffe39e]"
+            >
+              돌아가기
+            </Button>
+            <div className="flex flex-row gap-10">
+              <CancelModal id={applicationId}>
+                {(onOpen) => (
+                  <Button
+                    onClick={onOpen}
+                    className="border-1 p-16 border-orange rounded-[20px] w-120 h-40 bg-white text-[18px] text-orange"
+                  >
+                    신청 취소
+                  </Button>
+                )}
+              </CancelModal>
+
               <Button
-                onClick={onOpen}
-                className="border-1 p-16 border-orange rounded-[20px] w-120 h-40 bg-white text-[18px] text-orange"
+                onClick={() => push('/apply')}
+                className="border-1 p-16 rounded-[20px] w-120 h-40 text-[18px] text-white bg-gradient-to-r from-[#ffbb37] to-[#ffe39e]"
               >
-                신청 취소
+                수정
               </Button>
-            )}
-          </CancelModal>
-
-          <Button
-            onClick={() => push('/apply')}
-            className="border-1 p-16 rounded-[20px] w-120 h-40 text-[18px] text-white bg-gradient-to-r from-[#ffbb37] to-[#ffe39e]"
-          >
-            수정
-          </Button>
-        </div>
-      </If>
+            </div>
+          </div>
+        </If>
+      </div>
     </div>
   )
 }
