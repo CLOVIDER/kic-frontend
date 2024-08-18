b8c0ee368d5747b12bd76c7190cba4318e3cfc08
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-08-18 19:48:30 +0900

    feat: 데이터가 없는 경우 로직 추가

--- src/app/(user)/history/components/ChildResultCard/ChildResultCards.tsx
+++ src/app/(user)/history/components/ChildResultCard/ChildResultCards.tsx
@@ -9,13 +9,17 @@ export default function ChildResultCards() {
 
   return (
     <>
-      {childResult?.map(({ childName, lotteryResults }: ChildLottery) => (
-        <ChildResultCard
-          key={childName}
-          name={childName}
-          lotteryResults={lotteryResults}
-        />
-      ))}
+      {childResult && childResult.length > 0 ? (
+        childResult.map(({ childName, lotteryResults }: ChildLottery) => (
+          <ChildResultCard
+            key={childName}
+            name={childName}
+            lotteryResults={lotteryResults}
+          />
+        ))
+      ) : (
+        <p>신청 내역이 없습니다.</p>
+      )}
     </>
   )
 }
